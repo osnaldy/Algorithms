7d6a5883a8ce4771570f788317deaf246d590861
osnaldy <osnaldy@hotmail.com>
2018-01-31 22:21:57 -0500

    Created an algoritm that the returns the posible pieces of cake that can be given to a person based on the month and day they were born

---
+++ BirthdayChocolate.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/*
+Lily has a chocolate bar consisting of a row of n squares where each square has an integer written on it.
+She wants to share it with Ron for his birthday, which falls on month m and day d.
+Lily wants to give Ron a piece of chocolate only if it contains m consecutive squares whose integers sum to d.
+ */
+
+public class BirthdayChocolate {
+
+    static int solve(int n, int[] s, int d, int m){
+
+        int count = 0;
+
+        for (int i = 0; i < (s.length - m) + 1; i++){
+
+            if (s.length == m && s[0] == d){
+
+                count++;
+                System.out.println("Hello");
+                break;
+            }
+
+            else if ((s[i] + s[i+1]) == d){
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+
+        for (int i = 0; i < s.length; i++){
+
+            s[i] = in.nextInt();
+        }
+
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = solve(n, s, d, m);
+        System.out.println(result);
+    }
+}
